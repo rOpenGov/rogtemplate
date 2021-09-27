8be192257b80eba4518987c87975c9c4d58215af
dieghernan <diego.hernangomezherrero@gmail.com>
2021-09-27 10:58:20 +0200

    Fix autolinking

--- R/rog_add_template_pkgdown.R
+++ R/rog_add_template_pkgdown.R
@@ -2,11 +2,18 @@
 #'
 #' Add the corresponding configuration to your `_pkgdown.yml`. If it is not
 #' present, it would create a file on `pkgdown/_pkgdown.yml`. It would also
-#' add the corresponding lines to your .Rbuildignore
+#' add the corresponding lines to your .Rbuildignore.
+#'
+#' It also adds the corresponding urls to the `_pkgdown.yml` file and the
+#' DESCRIPTION file if not present:
+#' * https://ropengov.github.io/pkgname/
+#' * https://github.com/ropengov/pkgname
 #'
 #' @family extras
 #'
-#' @seealso `vignette("pkgdown", package = "pkgdown")`.
+#' @seealso `vignette("pkgdown", package = "pkgdown")`,
+#' `vignette("linking", package = "pkgdown")`,
+#' [usethis::use_tidy_description()].
 #'
 #' @export
 #' @examples
@@ -15,13 +22,43 @@
 #' }
 #'
 rog_add_template_pkgdown <- function() {
+  pkgname <- package_name()
+
+  pkgurl <- paste0("https://ropengov.github.io/", pkgname, "/")
+
+
+  # Add auto linking to description
+  desc_path <- file.path(normalizePath("."), "DESCRIPTION")
+
+  desc <- read.dcf(desc_path)
+
+  urls <- gsub("\n", "", desc[, "URL"])
+
+  urls <- unlist(strsplit(urls, split = ","))
+
+  # Add url if no present
+  if (isFALSE(pkgurl %in% tolower(urls))) urls <- c(urls, pkgurl)
+
+  # Add repo if no present
+  repo <- paste0("https://github.com/ropengov/", pkgname)
+  if (isFALSE(repo %in% tolower(urls))) urls <- c(urls, repo)
+
+  # Regenerate DESCRIPTION
+  desc[, "URL"] <- paste(urls, collapse = ", ")
+
+  write.dcf(desc, desc_path)
+  usethis::use_tidy_description()
+
   # template
-  template <- list(template = list(
-    package = "rogtemplate",
-    path = NULL,
-    params =
-      list(bootswatch = NULL)
-  ))
+  template <- list(
+    url = paste0("https://ropengov.github.io/", pkgname, "/"),
+    template = list(
+      package = "rogtemplate",
+      path = NULL,
+      params =
+        list(bootswatch = NULL)
+    )
+  )
 
 
   # Detect _pkgdown.yml
@@ -38,12 +75,15 @@ rog_add_template_pkgdown <- function() {
 
   if (file_exist) {
     newfile <- yaml::read_yaml(path)
-    newfile <- modifyList(newfile, template)
+    newfile <- modifyList(newfile, template, keep.null = FALSE)
+    newfile$template$path <- NULL
 
-    if (length(newfile$template$params) == 0) {
+    if (length(newfile$template$params) == 1 && "bootswatch"
+    %in% names(newfile$template$params)) {
       newfile <- modifyList(
         newfile,
-        list(template = list(params = NULL))
+        list(template = list(params = NULL)),
+        keep.null = FALSE
       )
     }
   } else {
